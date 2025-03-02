3d43059fb595be8d1bbd2d369187f253e88b5738
mkhmtolzhas <mkhmtcore@gmail.com>
2025-03-02 20:31:32 +0500

    added trigger on message

--- src/api/v1/whatsapp/router.py
+++ src/api/v1/whatsapp/router.py
@@ -22,3 +22,13 @@ async def send_voice(data: SendVoice):
     if not data.audio:
         raise WhatsAppException.InvalidAudio()
     return await service.send_audio(data.to, data.audio)
+
+
+@router.post("/on_message")
+async def on_message(data: SendMessage):
+    if not WhatsAppUtils.is_valid_phone_number(data.to):
+        raise WhatsAppException.InvalidPhoneNumber()
+    if not data.body:
+        raise WhatsAppException.InvalidMessage()
+    print(f"Message from {data.to}: {data.body}")
+    return {"status": "OK"}
